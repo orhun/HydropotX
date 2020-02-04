11991074d5846442fb8951b3ed7b76408018b3d8
orhun <orhunparmaksiz@gmail.com>
2020-02-04 16:46:18 +0300

    refactor: Organize the pin and constant definitions

--- HydropotX.h
+++ HydropotX.h
@@ -5,16 +5,15 @@
  * m = 7.4 - 5.5 / 3.51 - 10.11 = -0.28
  * b = 5.5 - (-0.28 x 10.11) = 8.3
  */
-#define PH_PIN A2
-/* m = (y2 - y1) / (x2 - x1) */
+/*
+ * m = (y2 - y1) / (x2 - x1)
+ * b = y - mx
+ */
 #define PH_CONST_M -0.28
-/* b = y - mx */
-#define PH_CONST_B 8.3 
-#define MOTOR1_PIN 2
-#define MOTOR2_PIN 3
-/* Hana      [USA]        PPM converion:   0.5  */
+#define PH_CONST_B 8.3
+/* Hana      [USA]        PPM converion:   0.50 */
 /* Eutech    [EU]         PPM conversion:  0.64 */
-/* Tranchen  [Australia]  PPM conversion:  0.7  */
+/* Tranchen  [Australia]  PPM conversion:  0.70 */
 #define PPM_CONV 0.5
 /* Resistance of the powering pins */
 #define EC_RA 25
@@ -22,12 +21,16 @@
 #define EC_R1 1000
 /* Constant for EC measurements */
 #define EC_CONST 2.11
-#define EC_PIN A0
-#define EC_GND A1
-#define EC_VCC A4
 /* Temperature compensation. */
 /* 0.019 is generally considered as the standard for plant nutrients. */
 #define TEMP_COMP 0.019
+/* Pins */
+#define PH_PIN A2
+#define EC_PIN A0
+#define EC_GND A1
+#define EC_VCC A4
 #define TEMP_DATA_PIN 10
 #define TEMP_VCC_PIN 8
 #define TEMP_GND_PIN 9
+#define MOTOR1_PIN 2
+#define MOTOR2_PIN 3
