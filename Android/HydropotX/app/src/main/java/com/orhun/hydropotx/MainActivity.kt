10ed5e2c4e6dd518e3d8d57dea3cd6390057b160
orhun <orhunparmaksiz@gmail.com>
2020-02-25 11:57:06 +0300

    fix: Use onActivityResult function for bluetooth permission

--- Android/HydropotX/app/src/main/java/com/orhun/hydropotx/MainActivity.kt
+++ Android/HydropotX/app/src/main/java/com/orhun/hydropotx/MainActivity.kt
@@ -1,48 +1,62 @@
 package com.orhun.hydropotx
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.bluetooth.BluetoothDevice
 import android.content.pm.PackageManager
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
+import android.os.Build
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+class MainActivity : Activity() {
+
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    companion object {
+        private val TAG = "HydropotX"
+        private val REQUEST_ENABLE_BT = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (!bluetoothAdapter.isEnabled) {
+        if (bluetoothAdapter == null) {
+
+        } else if (!bluetoothAdapter!!.isEnabled) {
             val btIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(btIntent, 1)
+            startActivityForResult(btIntent, REQUEST_ENABLE_BT)
         } else {
-            val pairedDevices = bluetoothAdapter.bondedDevices
+            val pairedDevices = bluetoothAdapter!!.bondedDevices
             if (pairedDevices.size > 0) {
                 for (device: BluetoothDevice in pairedDevices) {
-                    Log.d(device.address, device.name);
+                    Log.d(device.address, device.name)
                 }
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == 1) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this@MainActivity,
-                    getString(R.string.bt_permission_error), Toast.LENGTH_LONG).show()
-            } else {
-                //
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_ENABLE_BT ->
+                if (resultCode == RESULT_OK) {
+                    //
+                } else {
+                    Log.d(TAG, getString(R.string.bt_permission_error))
+                    Toast.makeText(this, R.string.bt_permission_error,
+                        Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+
         }
+
     }
+
 }
